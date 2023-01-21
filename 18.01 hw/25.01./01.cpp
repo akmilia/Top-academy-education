b35da03a1188218eab6001e827cdb8ac9d5d308d
akmilia <122699033+akmilia@users.noreply.github.com>
2023-01-21 14:54:44 +0300

    2

--- 18.01 hw/25.01./01.cpp
+++ 18.01 hw/25.01./01.cpp
@@ -67,4 +67,98 @@ int main()
    Human Anna("Anna", "a"); 
    
 
-}
+} 
+
+
+#include <iostream> 
+// #include <string> 
+
+// class Human
+// {
+//     std::string *name = new std::string("Ivan"); 
+//     std::string *surname = new std::string("Ivanov"); 
+
+//     public: 
+
+//     Human(std::string name,  std::string surname) //конструктор 
+//     {
+//         this->(*name) = name; 
+//         this->(*surname) = surname; 
+//     };  
+      
+
+//     void GetName() 
+//     {
+//         std::cout<<this->*name<<std::endl; 
+//     };  
+
+//     void GetSurname()
+//     {
+//       std::cout<<" "<<this->*surname<<std::endl;
+//     }; 
+    
+    
+//     void SetName(std::string name) 
+//     {
+//         this->(*name)=name; 
+//     } 
+    
+//     void SetSurname(std::string surname) 
+//     {
+//         this->(*surname)=surname; 
+//     }
+    
+//     ~Human()
+//     {
+//         delete name; 
+//         delete surname; 
+
+//     }
+// }; 
+
+// class Apartment 
+// {    
+//      int k =0; 
+//      Human *hum_list;  
+     
+//      public:
+
+//      Apartment(Human hum, int k) 
+//      {
+//         hum_list = new Human[k]; 
+//         for (int i=0; i<k; ++i) 
+//         hum_list[i]=hum[i]; 
+      
+//      };  
+     
+//      ~Apartment(){}; 
+     
+
+// }; 
+
+// class House
+// {
+//      int id; 
+//      int k =0; 
+//      Apartment *apar_list;  
+     
+//      public:
+
+//      House(Apartment apar, int k) 
+//      {
+//         apar_list = new Apartment[k]; 
+//         for (int i=0; i<k; ++i) 
+//         apar_list[i]=apar[i]; 
+      
+//      };  
+     
+//      ~Apartment(){}; 
+     
+// }; 
+
+// int main()
+// {
+//   Human Anna("Anna", "a"); 
+   
+
+// }
