8559399bd306f3918460dacdd47bb4a43961c6c1
akmilia <122699033+akmilia@users.noreply.github.com>
2023-01-23 17:48:31 +0000

    \just appartments

--- 25.01 hw/01.cpp
+++ 25.01 hw/01.cpp
@@ -4,12 +4,12 @@
 
 class Human
 { 
-   std::string *fullname;  
+   std::string fullname;  
    int age = 18;  
 
    public:   
    //конструктор по умолчанию
-   Human(){"Tanya Grotter", 18};
+   Human() : Human("Tanya Grotter", 18) {};
 
    //конструктор копирования 
    Human(const Human& other): Human(other.fullname, other.age){}
@@ -17,19 +17,17 @@ class Human
    Human(std::string fullname, int age )  
    {    
         std::cout<<"Fullname "; 
-        std::cin>>*(this->fullname); 
+        std::cin>>(this->fullname); 
         std::cout<<"Age "; 
         std::cin>>this->age; 
    }
    
-   ~Human(){
-    delete (this->fullname); 
-   } 
+   ~Human(){} 
 
-   std::string GetFullname() {return *fullname}; 
-   int GetAge(return age);
+   std::string GetFullname() {return fullname; }; 
+   int GetAge() {return age; };
 
-   void setName(std::string fullname) { *(this->fullname) = fullname; }
+   void setName(std::string fullname) { this->fullname = fullname; }
    void setAge(int age) { this->age = age; }
 }; 
 
@@ -38,43 +36,68 @@ class Apartment
      Human * people; 
      int cap;
      int fam; 
-     int num; 
+
 
      public: 
-     Apartment(){4, 1, 1} 
+     Apartment(){Human(), 0, 0 } 
      Apartment(const Apartment& other) 
-    {   this->people = new Human[fam];  
+    {   this->people = new Human[cap];  
         this->cap=other.cap; 
         for (int i=0; i<this->fam; ++i ) 
         {
-            this->people[i].fam = this->people[i].fam; 
-            this->people[i].num = this->people[i].num ; 
-
-
+            this->people[i] = Human(other.people[i].GetFullname(), other.people[i].GetAge()); 
+            this->people[i].setAge(other.people[i].GetAge());
         }
-        other.fam, other.num, other.people} 
+    }  
 
-     Apartment(int fam, int num) 
-     {
-         this->fam=fam; 
-         this->num=num; 
+      Apartment(Human people, int cap,  int fam) 
+     {   
          
-     }  
-     ~Apartment(){} 
-     
-     void add_to_arr(Human hum)
-     {
+         this->cap = cap; 
+         this->fam=fam; 
+     }     
+
+     void add_back(Human val) 
+      {   
+         if ((this->fam+1) > this->cap)
+          std::cout<<"No: cap = "<<this->cap<<" is too big\n"; 
+         else
+          { this->people[fam] = val; 
+         fam++; }
+     }    
+
+     void add_to_arr(int index, Human val)
+     {  
+        Human *tmp = new Human[fam - index]; 
+       
+        for(int i=index+1; i<this->fam; ++i) 
+        { 
+          tmp[i]=this->people[i]; 
+        } 
+         this->people[index]=val; 
+
+        for(int i=index+1; i<this->fam+1; ++i) 
+        { 
+         this->people[i] = tmp[i]; 
+        } 
 
      } 
 
-     Data(const Data &other) {
-        this->users = new User[other.size];
-        this->size = other.size;
-        for (int i = 0; i < this->size; ++i) {
-            this->users[i].setName(other.users[i].getName());
-            this->users[i].setAge(other.users[i].getAge());
-        }
+     void del(int index)     {
+    if (this->fam<= index)
+    std::cout<<"Nope\n"; //проверка
+    else
+    { for (int j=index; j<this->fam; j++) //обеспечивает перебор всех элементов массива
+    {
+        this->people[index-1] = this->people[index]; 
+    } 
+        fam--; 
     }
+   }
+     
+     void setHum(Human val) {this->Human} 
+     ~Apartment(){ delete []tmp; } 
+
 }; 
 
 class House
