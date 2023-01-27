f32172f2ce13c1dc45a3d49c1621748a50bea18f
akmilia <122699033+akmilia@users.noreply.github.com>
2023-01-27 23:48:49 +0000

    1

--- 25.01 hw/01.cpp
+++ 25.01 hw/01.cpp
@@ -51,11 +51,17 @@ class Apartment
         }
     }  
 
-      Apartment(Human people, int cap,  int fam) 
-     {   
-         
+      Apartment(Human *people, int cap,  int fam) 
+     {  
          this->cap = cap; 
          this->fam=fam; 
+        this->people = new Human[cap];  
+        for (int i=0; i<this->fam; ++i ) 
+        {
+            this->people[i] = people[i]; 
+        }
+         
+        
      }     
 
      void add_back(Human val) 
@@ -94,20 +100,119 @@ class Apartment
     } 
         fam--; 
     }
+   } 
+
+   void print()
+   {
+    for (int i=0; i<fam; ++i )
+    std::cout<<people[i]<<" "; 
+
+    std::cout<<"\n"; 
    }
      
-    /* void setHum(Human val, int i) {this->people[i].SetName(val.GetFullname()); }  */
+    int getSize()
+    {return fam; }  
+
+    int getCap() 
+    {return cap; }  
+
+    Human getHum(int i)
+    {return people[i]; }
 
      ~Apartment(){} 
 
 }; 
 
 class House
-{
+{ 
+    private: 
+    int id; 
+    int size; 
+    int cap; 
+    Apartment *podezd; 
 
-}; 
+    public: 
+    
+    House() {}; 
+
+    House(const House&  other) 
+    {
+        this->size = size; 
+        this->cap=cap; 
+        this->podezd = new Apartment [ cap]; 
+        
+         for (int i=0; i<this->size; ++i ) 
+        {
+            this->podezd[i] = Apartment(other.podezd[i].getSize(), other.podezd[i].getCap()); 
+            id++; 
+
+        }
+    }  
+
+    House(Apartment * podezd, int size, int cap)
+    {
+        this->cap = cap; 
+         this->size=size; 
+         id++; 
+        this->podezd = new Apartment [cap];  
+        for (int i=0; i<this->size; ++i ) 
+        {
+            this->podezd[i] = podezd[i]; 
+        }
+    } 
+
+    Apartment &operator[] (int index) 
+    {
+        return this->podezd [index]; 
+    }   
+
+    void print()
+   {
+    for (int i=0; i<size; ++i )
+    std::cout<<podezd[i]<<" "; 
+
+    std::cout<<"\n"; 
+   }  
+
+    int getId()
+    { return id; } 
+
+
+    ~House(){}; 
+
+
+};  
 
 int main()
-{
+{  
+    //only humans after all...
+    Human one; 
+    Human two("Misha", 15); 
+    Human tree("Pisha", 27); 
+    
+    Human tree_cop(tree); 
+    tree_cop.setName("Kisha"); 
+    std::cout<<tree_cop.GetFullname()<<"\n";  
+
+    //here we go to the apart-part  
+
+    Human *people = new Human[4] {one, two, tree, tree_cop}; 
+    /*Apartment <Human> peop(people, 4);*/ 
+
+    Apartment one_one; 
+    Apartment two_two(people, 8, 4); 
+    Apartment two_two_cop(two_two);  
+
+    two_two_cop.add_to_arr(2, Human("emae", 9)); 
+    two_two.del(0); 
+    std::cout<<"two_two_cop\n"; 
+    two_two_cop.print(); 
+    std::cout<<"two_two\n"; 
+    two_two.print();
+
+
+
+
+
 
 }
